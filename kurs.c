76650247f99d424b694138f29291c7c50f66d133
AlexandrDaultukaev <shyra31102000@mail.ru>
2020-05-21 15:16:06 +0700

    Add a greeting and condition for starting the game

--- kurs.c
+++ kurs.c
@@ -2,7 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-int print(int correct, int uncorrect)
+int begin(char run[4])
+{
+    while (strcmp(run, "run") != 0) {
+        printf("Keyboard Ninja. Напишите \"run\" для начала игры.\nЧтобы "
+               "закончить напишите \"end\"\n");
+        scanf("%s", run);
+    }
+    return 0;
+}
+
+void print(int correct, int uncorrect)
 {
     printf("Correct words: %d\nUncorrect words: %d\n", correct, uncorrect);
     exit(0);
@@ -10,32 +20,36 @@ int print(int correct, int uncorrect)
 
 int main()
 {
-    struct words {
+    typedef struct {
         char name[20];
-    };
-    struct words worrd;
+    } type;
+    char run[4];
+    type worrd;
     char i = 0;
     char word[20];
     char end[10] = "end";
     int uncorrect = 0;
     int correct = 0;
     FILE* f = fopen("words.txt", "r");
-    while (fscanf(f, "%s", worrd.name) != EOF) {
-        printf("\"%s\"\n", worrd.name);
-        printf("Write this word ");
-        scanf("%s", word);
-        if (strcmp(worrd.name, word) == 0) {
-            printf("Слово верно\n");
-            correct++;
-        } else {
-            if (strcmp(word, end) == 0) {
-                print(correct, uncorrect);
+    if (begin(run) == 0) {
+        while (fscanf(f, "%s", worrd.name) != EOF) {
+            printf("\"%s\"\n", worrd.name);
+            printf("Write this word ");
+            scanf("%s", word);
+            if (strcmp(worrd.name, word) == 0) {
+                printf("Слово верно\n");
+                correct++;
             } else {
-                printf("Слово неверно\n");
-                uncorrect++;
+                if (strcmp(word, end) == 0) {
+                    print(correct, uncorrect);
+                } else {
+                    printf("Слово неверно\n");
+                    uncorrect++;
+                }
             }
+            i++;
         }
-        i++;
+        print(correct, uncorrect);
     }
     return 0;
 }
